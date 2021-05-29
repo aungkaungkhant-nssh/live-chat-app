d3f181720724319587758d302c9558d7a32124db
aungkaungkhant-nssh <akkgit0909@gmail.com>
2021-05-29 11:44:02 +0630

    navbar style , get current user and composable function

---
+++ src/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav v-if="user">
+      <div>
+          <p>Hi {{user.displayName}}</p>
+          <p class="email">Logged in as {{user.email}}</p>
+      </div>
+      <button @click="logout">Logout</button>
+  </nav>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+import getUser from "../composables/getUser"
+export default {
+    setup(){
+        let {user}=getUser();
+        return {user}
+    }
+}
+</script>
+
+<style>
+    nav{
+    padding: 20px;
+    border-bottom: 1px solid #eee;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+nav p{
+    margin:2px auto;
+    font-size: 16px;
+    color:#444;
+}
+nav p.email{
+    font-size: 14px;
+    color:#999;
+}
+
+</style>
---
+++ src/composables/getUser.js
@@ -0,0 +1,10 @@
+const { ref } = require("@vue/reactivity");
+import { auth } from '../firebase/config'
+let user=ref(auth.currentUser);
+auth.onAuthStateChanged((_user)=>{
+    user.value=_user;
+})
+let getUser=()=>{
+    return {user};
+}
+export default getUser;
