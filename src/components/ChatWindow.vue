a85c1cdbbdd758f64dd44c97a288f029ee1e0857
aungkaungkhant-nssh <akkgit0909@gmail.com>
2021-05-30 11:31:34 +0630

    fetch message from firebase

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,19 +1,34 @@
 <template>
      <div class="chat-window">
-        <div class="messages">
+        <div class="messages" v-for="message in messages" :key="message.id">
             <div class="single">
                 <span class="created-at">3mins ago</span>
-                <span class="name">akk</span>
-                <span class="message">hi there man</span>
+                <span class="name">{{message.name}}</span>
+                <span class="message">{{message.message}}</span>
+               
             </div>
         </div>
     </div>
 </template>
 
 <script>
-export default {
+import { ref } from '@vue/reactivity';
+import { db } from '../firebase/config';
 
-}
+export default {
+    setup(){
+            let messages=ref([]);
+        db.collection("messages").orderBy("created_at").onSnapshot((snap)=>{
+              
+                snap.docs.forEach(doc =>{
+                    let document={...doc.data(),id:doc.id};
+                   messages.value.push(document);
+                })
+        })
+        
+        return{messages};
+    }
+}   
 </script>
 
 <style>
