82bc98c519054778ed72e15e0371e08119ea6a49
aungkaungkhant-nssh <akkgit0909@gmail.com>
2021-05-30 09:15:50 +0630

    chatForm design

---
+++ src/components/NewChatForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <form>
+      <textarea  v-model="message" placeholder="Text Message and hint enter" @keypress.prevent.enter="handleSubmit"></textarea>
+  </form>
+
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+import { db } from '../firebase/config';
+export default {
+    setup(){
+        let message=ref("");
+        let handleSubmit=()=>{
+            console.log("hi");
+        //    await db.collection("messages").add()
+        }
+        return{message,handleSubmit}
+    }
+}
+</script>
+
+<style>
+  form{
+        margin: 10px;
+    }
+    textarea{
+        width: 100%;
+        max-width: 100%;
+        margin-bottom: 6px;
+        padding:10px;
+        box-sizing: border-box;
+        border: 0;
+        border-radius: 20px;
+        font-family: inherit;
+        outline: none;
+    }
+</style>
--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="container">
       <Navbar></Navbar>
+      <NewChatForm></NewChatForm>
   </div>
 </template>
 
 <script>
+import NewChatForm from '../components/NewChatForm'
 import Navbar from '../components/Navbar'
 export default {
-  components: { Navbar },
+  components: {
+    NewChatForm, Navbar },
 
 }
 </script>
