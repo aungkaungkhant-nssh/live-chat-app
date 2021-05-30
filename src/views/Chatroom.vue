163e479f1646440acfa4aa65ca47f074177cee57
aungkaungkhant-nssh <akkgit0909@gmail.com>
2021-05-30 10:14:50 +0630

    watching current user

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="container" v-if="user">
       <Navbar></Navbar>
       <NewChatForm></NewChatForm>
   </div>
@@ -8,11 +8,23 @@
 <script>
 import NewChatForm from '../components/NewChatForm'
 import Navbar from '../components/Navbar'
+import getUser from '../composables/getUser'
+import { watch } from '@vue/runtime-core'
+import {useRouter} from "vue-router"
 
 export default {
   components: {
     NewChatForm, Navbar },
-   
+    setup(){
+      let {user}=getUser();
+      let router=useRouter();
+      watch(user,()=>{
+        if(!user.value){
+          router.push("/")
+        }
+      })
+      return{user}
+    }
 }
 </script>
 
